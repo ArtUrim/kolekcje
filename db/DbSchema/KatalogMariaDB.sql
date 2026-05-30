b16cf7dae8c2b70a013391a1aa732a91acc87c88
Artur <lozinski.artur@gmail.com>
2026-05-30 11:12:32 +0200

    create mariadb container for easy start with new empty DB

--- db/DbSchema/KatalogMariaDB.sql
+++ db/DbSchema/KatalogMariaDB.sql
@@ -6,7 +6,7 @@ SET foreign_key_checks = 0;
 
 SET NAMES utf8mb4;
 
-CREATE DATABASE `katalog` IF NOT EXISTS `katalog`
+CREATE DATABASE IF NOT EXISTS `katalog`;
 USE `katalog`;
 
 CREATE TABLE `Authors` (
